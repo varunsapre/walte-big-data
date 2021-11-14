4d36a0a4cef6fb586e3e44068c0bb9e5152494bf
varunsapre <varun.sapre@gmail.com>
2021-11-14 00:02:26 -0800

    added logging and easy path spec

--- spark/read-yelp.py
+++ spark/read-yelp.py
@@ -1,13 +1,28 @@
 import pyspark
 from pyspark.sql import SparkSession
+import logging as l
+l.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s', level=l.INFO)
+
+FILESYSTEM = "file://" # localfile needs 'file://'
+HDFS_NODE = "localhost:9000" # hostname and namenode port, default = 9000
+HDFS_PATH = "/input/yelp/yelp_academic_dataset_review.json" # include first '/' but not last, use entire path if accessing local file
+
+
+# if not hdfs, disregard node variable
+if "hdfs" not in FILESYSTEM:
+    HDFS_NODE = ""
+
+FILE_ACCESS = f"{FILESYSTEM}{HDFS_NODE}{HDFS_PATH}"
 
 
 spark = SparkSession.builder.appName('local[*]').getOrCreate()
-
 sc = spark.sparkContext
 sc.setLogLevel("WARN")
+l.info("-----SPARK SESSION STARTED-----")
 
-# df = spark.read.json("hdfs://localhost:9000/input/yelp/trial.json")
-df = spark.read.json("file:///tmp/trial.json")
+l.info(f"Accesing file: {FILE_ACCESS}")
+df = spark.read.json(FILE_ACCESS)
+l.info("file read complete")
+l.info(f"file details - rows: {df.count()}, columns: {len(df.columns)}")
 df.printSchema()
-df.show()
+# df.show()
